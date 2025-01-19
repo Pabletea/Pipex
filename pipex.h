b6ef7165df39ad0070f9beccfb2b95fecfd12d7f
Pablo <pabloalonsogarc@gmail.com>
2025-01-19 17:42:49 +0100

    pipex started

---
+++ pipex.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pabalons <pabalons@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/19 11:59:33 by pabalons          #+#    #+#             */
+/*   Updated: 2025/01/19 17:34:18 by pabalons         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_libft/libft.h"
+#include "stdlib.h"
+#include "unistd.h"
+#include "stdio.h"
+
+int open_file(char *file, int n);
+void print_error(char *s);
+
